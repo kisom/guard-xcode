0f8eb5ed6109e07c5c82bfffcc4e069cf03110a9
Kyle Isom <coder@kyleisom.net>
2012-03-16 21:54:56 +0300

    run tests in more situations

--- lib/guard/xcode.rb
+++ lib/guard/xcode.rb
@@ -14,6 +14,13 @@ module Guard
       @target = options[:target]
       @config = options[:configuration]
       @scheme = options[:scheme]
+      @arch = options[:arch]
+      
+      if true == options[:all]
+        @all = true
+      else
+        @all = false
+      end
 
       if true == options[:quiet]
         @quiet = true
@@ -21,11 +28,68 @@ module Guard
         @quiet = false
       end
 
-      if true == options['clean']
+      if true == options[:clean]
         @clean = true
       else
         @clean = false
       end
+    end
+
+    def build
+      res = true
+      build_line = 'xcodebuild '
+
+      ## configure build options
+      unless nil == @configuration
+        build_line += "-configuration #{@configuration} "
+      end
+
+      unless nil == @target
+        build_line += "-target #{@target} "
+      end
+
+      if @all
+        build_line += "-alltargets "
+      end
+
+      unless nil == @scheme
+        build_line += "-scheme #{@scheme} "
+      end
+
+      unless nil == @arch
+        build_line += "-arch #{@arch} "
+      end
+      
+      unless false == @clean
+        build_line += "clean "
+      end
+
+      build_line += "build"
+
+      ## run the build
+      unless @quiet
+        Notifier.notify("kicking off build with:\n#{build_line}")
+      end
+
+      output = `#{build_line}`
+
+      unless @quiet
+        Notifier.notify("build finished.")
+      end
+
+      puts output
+
+      if output =~ /error/
+        Notifier.notify("xcode: errors in build!")
+        res = false
+      end
+
+      if output =~ /warning/
+        Notifier.notify("xcode: warnings in build!")
+        res = false
+      end
+
+      return res
     end
 
     # Call once when Guard starts. Please override initialize method to init stuff.
@@ -48,58 +112,21 @@ module Guard
     # This method should be principally used for long action like running all specs/tests/...
     # @raise [:task_has_failed] when run_all has failed
     def run_all
+      return build
     end
 
     # Called on file(s) modifications that the Guard watches.
     # @param [Array<String>] paths the changes files or paths
     # @raise [:task_has_failed] when run_on_change has failed
     def run_on_change(paths)
-      build_line = 'xcodebuild '
-
-      unless nil == @configuration
-        build_line += "-configuration #{@configuration} "
-      end
-
-      unless nil == @target
-        build_line += "-target #{@target} "
-      end
-
-      unless nil == @scheme
-        build_line += "-scheme #{@scheme} "
-      end
-
-      unless false == @clean
-        build_line += "clean "
-      end
-
-      build_line += "build"
-
-      unless @quiet
-        Notifier.notify("kicking off build with:\n#{build_line}")
-      end
-
-      output = `#{build_line}`
-
-      unless @quiet
-        Notifier.notify("build finished.")
-      end
-
-      puts output
-
-      if output =~ /error/
-        Notifier.notify("xcode: errors in build!")
-      end
-
-      if output =~ /warning/
-        Notifier.notify("xcode: warnings in build!")
-      end
-
+      return build
     end
 
     # Called on file(s) deletions that the Guard watches.
     # @param [Array<String>] paths the deleted files or paths
     # @raise [:task_has_failed] when run_on_change has failed
     def run_on_deletion(paths)
+      return build
     end
 
   end
