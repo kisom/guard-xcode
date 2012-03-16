5543e6316e2d4c68aaa7578355825f83f1fcd645
Kyle Isom <coder@kyleisom.net>
2012-03-17 00:13:33 +0300

    Add reference main files

---
+++ test/data/main_error.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  GuardXcodeTest
+//
+//  Created by Kyle Isom on 16/03/2012.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        foo
+        
+    }
+    return 0;
+}
+
---
+++ test/data/main_warnings_error.m
@@ -0,0 +1,23 @@
+//
+//  main.m
+//  GuardXcodeTest
+//
+//  Created by Kyle Isom on 16/03/2012.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        int foo;
+        foo
+        
+    }
+    return 0;
+}
+
